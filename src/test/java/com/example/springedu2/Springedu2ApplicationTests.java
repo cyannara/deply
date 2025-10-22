17640286f3ae231d56b70922695f4dd6959c8bb6
cyannara <cyannara@naver.com>
2025-10-22 13:58:41 +0900

    test 파일 추가

--- src/test/java/com/example/springedu2/Springedu2ApplicationTests.java
+++ src/test/java/com/example/springedu2/Springedu2ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.springedu2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.inject.Named;
 
 @SpringBootTest
 class Springedu2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  @Named("사원등록")
+  void contexttest1() {
+    assertThat("a").isEqualTo("a");
+  }
 
+  @Test
+  @Named("사원조회")
+  void contexttest2() {
+    assertThat("a").isEqualTo("1");
+  }
 }
